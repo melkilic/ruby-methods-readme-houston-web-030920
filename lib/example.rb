b6a4763b6feb1e23f2d864b4a897ef995772cb15
Melike KILIC <melike180400@gmail.com>
2020-03-09 20:45:58 +0000

    Automatically backed up by Learn

--- lib/example.rb
+++ lib/example.rb
@@ -1,17 +1,13 @@
 # Use this space to code along with the readme
 
-def say_hello_world_ten_times
- phrase = "Hello World!"
- puts phrase
- puts phrase
- puts phrase
- puts phrase
- puts phrase
- puts phrase
- puts phrase
- puts phrase
- puts phrase
- puts phrase
-end
-
-say_hello_world_ten_times
+phrase = "Hello World!"
+puts phrase
+puts phrase
+puts phrase
+puts phrase
+puts phrase
+puts phrase
+puts phrase
+puts phrase
+puts phrase
+puts phrase
